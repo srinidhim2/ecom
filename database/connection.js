28fa792c7f442fed71b773612e00accd73d31982
Srinidhi M <65710007+srinidhim2@users.noreply.github.com>
2023-10-30 19:22:25 +0530

    done with handling uncaught exceptions and promises and error logging

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { DB_URL } = process.env;
 
+
 async function createConnection() {
 
     try {
--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,5 +1,7 @@
+const { logger } = require('../logger/logger');
 process.on('uncaughtException', (err) => {
     console.log("uncaughtException");
+    logger.error(err.message, err);
 })
 process.on('unhandledRejection', (err) => {
     console.log('unhandledRejection');
@@ -9,6 +11,7 @@ process.on('unhandledRejection', (err) => {
 function handleErrors(err, req, res, next) {
     try {
         if (res.statusCode == 200) {
+            logger.error(err.message, err);
             res.status(500);
         }
         res.json({ error: err.message || "something went wrong" });
