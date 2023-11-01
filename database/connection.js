ed79d614dbaf0a874ad236aabce4064d335b1382
Srinidhi M <65710007+srinidhim2@users.noreply.github.com>
2023-11-01 14:11:21 +0530

    ignore_2

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb+srv://admin:adminadmin@ecom.d8tmrio.mongodb.net/?retryWrites=true&w=majority';
+const { DB_URL } = process.env;
 
 async function createConnection() {
     try {
